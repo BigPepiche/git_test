909377188a701da9b7dc46b3e5e8e8aceeb7e921
BigPepiche <reggiehr_30@hotmail.com>
2024-11-04 20:25:54 -0500

    V2 de installSoftware

--- install-softwares.sh
+++ install-softwares.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash 
 # install VS Code, Python, systemd
 # Update the package list
 sudo apt update -y
@@ -10,17 +10,17 @@ sudo apt install code -y
 sudo apt install python3 -y
 
 # Install Systemd
-sudo apt install systemd
+sudo apt install systemd -y
 
 # Script to install the latest Docker and Docker Compose on Debian-based systems
 sudo apt install curl -y
 curl -fsSL test.docker.com -o get-docker.sh && sh get-docker.sh
-sudo apt install docker-compose 
+sudo apt install docker-compose -y
 
 # Installing git and cloning the backend
-sudo apt install git
+sudo apt install git -y
 cd ..
-git clone https://git.aquapoly.ca/Aquapoly/Aquadash-backend.git
+git clone https://git.aquapoly.ca/Aquapoly/Aquadash-backend.git ../
 
 
 
