e03ee697a3c02b61bf8bec54137a43c7c73a70e9
BigPepiche <reggiehr_30@hotmail.com>
2024-11-19 14:24:07 -0500

    install-software updates

--- install-softwares.sh
+++ install-softwares.sh
@@ -1,6 +1,19 @@
 #!/bin/bash 
 # install VS Code, Python, systemd
 # Update the package list
+set -e
+
+clear
+
+echo "Bienvenue sur Aquadash"
+echo " "
+echo "Entrez votre mot de passe pour commencer l'installation"
+
+sudo clear
+echo "Début de l'installation. Cela devrait prendre quelques minutes"
+echo "Des messages vont s'afficher dans le terminal sous peu, ne pas s'en inquiéter"
+sleep 4
+
 sudo apt update -y
 
 # Install VS Code
@@ -17,11 +30,36 @@ sudo apt install curl -y
 curl -fsSL test.docker.com -o get-docker.sh && sh get-docker.sh
 sudo apt install docker-compose -y
 
+
+
 # Installing git and cloning the backend
 sudo apt install git -y
+
+# Pulling sensors
 cd ..
-git clone https://git.aquapoly.ca/Aquapoly/Aquadash-backend.git ../
+clear
+echo "C'est presque terminé, veuillez s'il vous plait entrer vos informations d'autentification git tea" 
+echo " "
+git clone https://git.aquapoly.ca/Aquapoly/Sensors.git s
+
+# Pulling Backend
+clear
+echo "Encore une fois" 
+echo " "
+git clone https://git.aquapoly.ca/Aquapoly/Aquadash-backend.git 
+
+python3 -m venv .venv
 
 
+clear 
 
-reboot
+echo "Installation réussie, il est conseillé de redémarer l'ordinateur"
+
+while true; do
+    read -r -p "Voulez vous redémarer maintenant? (O/n) " answer
+    case $answer in
+        [oO]* ) clear ;echo "Redémarage..."; sleep 5; reboot; break;;
+        [Nn]* ) exit;;
+        * ) echo "Veuiller entrer O ou N s'il vous plait";;
+    esac
+done
